6ab3c77e27a002594c44cfa62928b264aa617b3c
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-05-16 22:55:11 +0800

    新增 时间计算

--- src/components/Work/Discuss/Discuss.tsx
+++ src/components/Work/Discuss/Discuss.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Badge } from 'antd';
+import { Badge, Col, Row, Progress } from 'antd';
+import discussData from '../../../test/discuss';
 
 export interface Props {
 
@@ -18,15 +19,56 @@ class Discuss extends React.Component<Props, State> {
     statusPoint = (status: number, withText: boolean) => {
         switch (status) {
             case 1:
-                return <Badge status="success" text={withText ? "运行中" : null} />
+                return <Badge status="success" text={withText ? "进行中" : null} />
             case 2:
                 return <Badge status="warning" text={withText ? "状态不明" : null} />
             case 3:
-                return <Badge status="error" text={withText ? "即将完结" : null} />
+                return <Badge status="error" text={withText ? "已完结" : null} />
             case 4:
                 return <Badge status="default" text={withText ? "未开始" : null} />
 
         }
+    }
+
+    calcProgress = (startTime: string, endTime: string) => {
+        let start = new Date(startTime);
+        let end = new Date(endTime);
+        let now = new Date();
+        if (now.getTime() - end.getTime() < 0) {
+            return Math.ceil((now.getTime() - start.getTime()) / (end.getTime() - start.getTime()) * 100)
+        } else {
+            return 100
+        }
+
+    }
+
+    renderItem = () => {
+        const data = discussData.data;
+        const list: JSX.Element[] = [];
+        data.map((item) => {
+            list.push(
+                <Col span={12} >
+                    <div className="discuss-item card-shadow">
+                        <div>
+                            <span className="discuss-item-title">{this.statusPoint(item.status, false)}{item.title}</span>
+                            <div className="discuss-item-content">
+                                <div>
+                                    {item.status === 4 ? null : <span className="discuss-item-text1">团队：{item.team}</span>}
+                                    <span className="discuss-item-text1">组建人：{item.host}</span>
+                                </div>
+                                <div>
+                                    {item.status === 4 ? <span className="discuss-item-text2">项目开始时间：{item.startTime}</span> : null}
+                                    {item.status === 4 ? null : <span className="discuss-item-text2">最近编辑者：{item.lastEditor}</span>}
+                                    {item.status === 4 ? null : <span className="discuss-item-text2">最近编辑于：{item.lastEditTime}</span>}
+                                </div>
+                            </div>
+                        </div>
+                        <Progress type="dashboard" percent={this.calcProgress(item.startTime, item.endTime)} />
+                    </div>
+                </Col>
+            )
+        })
+        return list;
     }
 
     render() {
@@ -39,6 +81,9 @@ class Discuss extends React.Component<Props, State> {
                     {this.statusPoint(3, true)}
                     {this.statusPoint(4, true)}
                 </p>
+                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }} className="discuss-items">
+                    {this.renderItem()}
+                </Row>
             </div>
         );
     }
---
+++ src/test/discuss.ts
@@ -0,0 +1,39 @@
+export default {
+    "data": [{
+        "title": "天外天at Pro系统运营",
+        "status": 1,
+        "team": "舍我其谁",
+        "host": "彭某某",
+        "lastEditor": "王某",
+        "lastEditTime": "2020-05-02 17:32",
+        "startTime": "2020-04-08 11:05",
+        "endTime": "2020-05-20 11:05"
+    }, {
+        "title": "工作室二十周年庆典筹备",
+        "status": 4,
+        "team": "",
+        "host": "王某某",
+        "lastEditor": "",
+        "lastEditTime": "",
+        "startTime": "2020-08-05 11:05",
+        "endTime": "2020-08-10 11:05"
+    }, {
+        "title": "关于微北洋的百种修复方法",
+        "status": 3,
+        "team": "一马当先",
+        "host": "李某某",
+        "lastEditor": "李某某",
+        "lastEditTime": "2020-05-01 00:14",
+        "startTime": "2020-04-01 11:05",
+        "endTime": "2020-05-20 11:05"
+    }, {
+        "title": "关于新型算法结构的探究",
+        "status": 2,
+        "team": "迷惑行为",
+        "host": "温某某",
+        "lastEditor": "温某某",
+        "lastEditTime": "2019-02-03 00:00",
+        "startTime": "2020-04-20 11:05",
+        "endTime": "2020-05-20 11:05"
+    }]
+}
