a911a55cc9fb4dfde1e7abc1cd8bcb1bcb1daa6d
CALLMELARE <zuoyunfei@twt.edu.cn>
2020-05-31 16:41:02 +0800

    修复 登出

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -3,27 +3,32 @@ import { CheckCircleOutlined } from '@ant-design/icons';
 import React, { Component } from 'react';
 import fetchApi from '../api/callApi';
 import { getLogout } from '../api/OAuth';
+import icon from '../assets/twtlogo_tilt.svg';
+import '../styles/login.scss';
 
 class Logout extends Component {
 
     componentDidMount = () => {
-        const {apiPath,request} =getLogout();
-        fetchApi(apiPath,request)
-        .then(res=>res.json())
-        .then(data=>{
-            
-        })
+        const { apiPath, request } = getLogout();
+        fetchApi(apiPath, request)
+            .then(res => res.json())
+            .then(data => {
+
+            })
     }
 
     render() {
         sessionStorage.clear();
         return (
-            <Result
-                icon={<CheckCircleOutlined />}
-                title="你已成功登出"
-                status={"success"}
-                extra={<Button type="primary" href="/at-pro">返回</Button>}
-            />
+            <div className="ant-layout App">
+                <div className="logout-card">
+                    <p>客官慢走，欢迎下次光临</p>
+                    <span>See you next time</span>
+                    <a href="/at-pro/">
+                        <img src={icon} alt="logo" width={70} height={70} />
+                    </a>
+                </div>
+            </div>
         )
     }
 }
